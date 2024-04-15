a604e80a6fdd99b4c4bfe0ff0711947096781d30
Olly Headey <olly@headey.org>
2024-04-15 11:38:05 +0100

    Follow proper Rails conventions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,6 @@
-import { Application } from "@hotwired/stimulus"
-import "@rails/actiontext"
-
-const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
 
 import "rhino-editor"
 import "rhino-editor/exports/styles/trix.css"
